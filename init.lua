d4e49281c844ee858a529cca5471da40b785a9d1
Abbas0024 <abbas24.amiruddin@gmail.com>
2024-12-02 22:57:17 +0530

    removed rustaceanvim plugin

--- init.lua
+++ init.lua
@@ -924,7 +924,6 @@ require('lazy').setup({
   require 'kickstart.plugins.neo-tree',
   require 'kickstart.plugins.gitsigns', -- adds gitsigns recommend keymaps
   require 'kickstart.plugins.catppuccin',
-  require 'kickstart.plugins.rustaceanvim',
 }, {
   ui = {
     -- If you are using a Nerd Font: set icons to an empty table which will use the
