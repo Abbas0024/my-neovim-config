9d7b0262d5295faeaff3661b4b6703030982fe10
Abbas0024 <abbas24.amiruddin@gmail.com>
2025-04-10 12:30:57 +0530

    added hidden files opening

--- init.lua
+++ init.lua
@@ -449,6 +449,10 @@ require('lazy').setup({
       vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
       vim.keymap.set('n', '<leader><leader>', builtin.buffers, { desc = '[ ] Find existing buffers' })
 
+      vim.keymap.set('n', '<leader>fh', function()
+        require('telescope.builtin').find_files { hidden = true }
+      end, { desc = '[F]ind [H]idden Files' })
+
       -- Slightly advanced example of overriding default behavior and theme
       vim.keymap.set('n', '<leader>/', function()
         -- You can pass additional configuration to Telescope to change the theme, layout, etc.
